7d5d03f345f556f48c0ef0ea0ee5c878bebe7bec
ilonavarro <ilo.navarro+github@gmail.com>
2022-08-26 07:33:29 +0100

    Finished :D

--- aluroni/src/pages/Cardapio/Buscador/index.tsx
+++ aluroni/src/pages/Cardapio/Buscador/index.tsx
@@ -10,7 +10,11 @@ interface Props {
 export default function Buscador({ busca, setBusca }: Props) {
   return (
     <div className={styles.buscador}>
-      <input value={busca} onChange={event => setBusca(event.target.value)} />
+      <input
+        value={busca}
+        onChange={event => setBusca(event.target.value)}
+        placeholder='Buscar'
+      />
       <CgSearch size={20} color='#4C4D5E' />
     </div>
   )
