d2237b01a57daf9f533abceb92967b6fc8f69e0c
ilonavarro <ilo.navarro+github@gmail.com>
2022-08-31 13:14:31 +0100

    Using ESLint and React Router Dom - Finished

--- aluroni/src/index.tsx
+++ aluroni/src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'normalize.css'
 import './index.css'
-import Cardapio from './pages/Cardapio'
+import Routes from './routes'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <Cardapio />
+    <Routes />
   </React.StrictMode>
 )
