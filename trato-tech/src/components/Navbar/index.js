1dc21f0712708faf82f50e26719e5ef251ff9591
ilonavarro <ilo.navarro+github@gmail.com>
2023-01-09 16:35:07 +0000

    trato-tech 25%

---
+++ trato-tech/src/components/Navbar/index.js
@@ -0,0 +1,42 @@
+import styles from './Navbar.module.scss'
+import { ReactComponent as Logo } from '../../assets/logo.svg'
+import classNames from 'classnames'
+import { RiShoppingCart2Line, RiShoppingCartFill } from 'react-icons/ri'
+import Busca from 'components/Busca'
+
+const iconeProps = {
+  color: 'white',
+  size: 24
+}
+
+export default function Navbar() {
+  return (
+    <nav className={styles.nav}>
+      <Logo className={styles.logo} />
+      <div className={styles.links}>
+        <div>
+          <a
+            href='/'
+            className={classNames(styles.link, {
+              [styles.selected]: window.location.pathname === '/'
+            })}
+          >
+            Página Inicial
+          </a>
+        </div>
+      </div>
+      <div className={styles.busca}>
+        <Busca />
+      </div>
+      <div className={styles.icones}>
+        <a href='/carrinho'>
+          {window.location.pathname === '/carrinho' ? (
+            <RiShoppingCartFill {...iconeProps} />
+          ) : (
+            <RiShoppingCart2Line {...iconeProps} />
+          )}
+        </a>
+      </div>
+    </nav>
+  )
+}
