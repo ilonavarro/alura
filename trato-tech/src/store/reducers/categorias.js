2998b0af049c366db14a5611650d569d6f527a5f
ilonavarro <ilo.navarro+github@gmail.com>
2023-01-21 18:41:09 +0000

    69% - Middleware with Redux Thunk

--- trato-tech/src/store/reducers/categorias.js
+++ trato-tech/src/store/reducers/categorias.js
@@ -1,17 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import categoriasService from 'services/categorias'
 
 const initialState = []
+
+export const buscarCategorias = createAsyncThunk(
+  'categorias/buscar',
+  categoriasService.buscar
+)
 
 const categoriasSlice = createSlice({
   name: 'categorias',
   initialState,
-  reducers: {
-    adicionarCategorias: (state, { payload }) => {
-      state.push(...payload)
-    }
+  extraReducers: builder => {
+    builder.addCase(buscarCategorias.fulfilled, (state, { payload }) => {
+      return payload
+    })
   }
 })
-
-export const { adicionarCategorias } = categoriasSlice.actions
 
 export default categoriasSlice.reducer
--- trato-tech/src/store/reducers/itens.js
+++ trato-tech/src/store/reducers/itens.js
@@ -1,5 +1,8 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import itensService from 'services/itens'
 import { v4 as uuid } from 'uuid'
+
+export const buscarItens = createAsyncThunk('itens/buscar', itensService.buscar)
 
 const itensSlice = createSlice({
   name: 'itens',
@@ -21,14 +24,15 @@ const itensSlice = createSlice({
     deletarItem: (state, { payload }) => {
       const index = state.findIndex(item => item.id === payload)
       state.splice(index, 1)
-    },
-    adicionarItens: (state, { payload }) => {
-      state.push(...payload)
     }
+  },
+  extraReducers: builder => {
+    builder.addCase(buscarItens.fulfilled, (state, { payload }) => {
+      return payload
+    })
   }
 })
 
-export const { mudarFavorito, cadastrarItem, mudarItem, deletarItem, adicionarItens } =
-  itensSlice.actions
+export const { mudarFavorito, cadastrarItem, mudarItem, deletarItem } = itensSlice.actions
 
 export default itensSlice.reducer
