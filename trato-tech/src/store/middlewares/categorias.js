7f641f45f83a57aad879b6c17a39456a0e504303
ilonavarro <ilo.navarro+github@gmail.com>
2023-01-22 16:38:13 +0000

    Middleware listener with bug

---
+++ trato-tech/src/store/middlewares/categorias.js
@@ -0,0 +1,11 @@
+import { createListenerMiddleware } from '@reduxjs/toolkit'
+import { carregarCategorias } from 'store/reducers/categorias'
+
+export const listener = createListenerMiddleware()
+
+listener.startListening({
+  actionCreator: carregarCategorias,
+  effect: async action => {
+    console.log('Buscando categorias: ', action)
+  }
+})
