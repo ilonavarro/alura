080d3b3c09c374280c540d116fa7f4a18d35db96
ilonavarro <ilo.navarro+github@gmail.com>
2023-01-29 18:17:06 +0000

    Tailwind + Vite finished

--- alura-newsletter/src/components/Article/Article.jsx
+++ alura-newsletter/src/components/Article/Article.jsx
@@ -1,20 +1,20 @@
 const Article = ({ title, text, tags, image, alt }) => {
   return (
-    <div className='p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex gap-2 flex-col items-center'>
+    <div className='alura-card'>
       <h3 className='text-xl text-alura-200 dark:text-gray-200 font-bold'>{title}</h3>
       <div className='w-full justify-end gap-2 pr-5 hidden sm:flex'>
         {tags.map(tag => (
-          <span
-            key={tag}
-            className='bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 font-bold text-xs uppercase'
-          >
+          <span key={tag} className='alura-tag'>
             {tag}
           </span>
         ))}
       </div>
       <div className='grid gap-1'>
         {text.map((content, index) => (
-          <p key={index} className='text-alura-200 dark:text-gray-400'>
+          <p
+            key={index}
+            className='text-alura-200 dark:text-gray-400 line-clamp-2 sm:line-clamp-none'
+          >
             {content}
           </p>
         ))}
