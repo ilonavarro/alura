1ae91d1fd926aa73dac29c87872fdd2e03ea116c
ilonavarro <ilo.navarro+github@gmail.com>
2023-02-02 17:57:14 +0000

    Testes feito de transferencia e deposito, encontrado erro em utils, corrigido

--- bytebank-testes/src/utils/utils.test.js
+++ bytebank-testes/src/utils/utils.test.js
@@ -1,3 +1,29 @@
+import { calculaNovoSaldo } from './index'
+
+describe('Quando eu realizo uma transação', () => {
+  test('que é um depósito, o saldo deve aumentar', () => {
+    const transacao = {
+      transacao: 'Depósito',
+      valor: 50,
+    }
+
+    const novoSaldo = calculaNovoSaldo(transacao, 100)
+
+    expect(novoSaldo).toBe(150)
+  })
+
+  test('que é uma transferência, o saldo deve diminuir', () => {
+    const transacao = {
+      transacao: 'Transferência',
+      valor: 50,
+    }
+
+    const novoSaldo = calculaNovoSaldo(transacao, 100)
+
+    expect(novoSaldo).toBe(50)
+  })
+})
+
 describe('Deve retornar o valor', () => {
   test('do saldo atualizado com o rendimento', () => {
     const calculaRendimento = jest.fn((saldo) => saldo + saldo * 0.005)
