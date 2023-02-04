dfadba18ae4a7357c06797b3cf9f7c26a0453584
ilonavarro <ilo.navarro+github@gmail.com>
2023-02-04 22:59:55 +0000

    alurabook-ds Iniciando o projeto

--- alurabooks-ds/src/componentes/AbBotao/index.tsx
+++ alurabooks-ds/src/componentes/AbBotao/index.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const BotaoEstilizado = styled.button`
+  background: #eb9b00;
+  padding: 16px 32px;
+  border: 2px solid #eb9b00;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+
+  &:hover {
+    background: #b87900;
+    border: 2px solid #b87900;
+  }
+`;
 
 export const AbBotao = () => {
-  return <button>Clique Aqui!</button>;
+  return <BotaoEstilizado>Clique Aqui!</BotaoEstilizado>;
 };
