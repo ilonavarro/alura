75a96caeb5d6e5b5b4d188dd5901771b29d95dd8
ilonavarro <ilo.navarro+github@gmail.com>
2023-02-23 17:19:08 +0000

    react-alurabooks: Pedido with style and delete

--- curso-react-alurabooks-main/src/paginas/Pedidos/index.tsx
+++ curso-react-alurabooks-main/src/paginas/Pedidos/index.tsx
@@ -1,26 +1,67 @@
 import { AbBotao } from 'ds-alurabooks'
+import axios from 'axios'
+import './Pedidos.css'
+import { useEffect, useState } from 'react'
+import { useObterToken } from '../../hooks'
+import { IPedido } from '../../interfaces/IPedido'
 
 const Pedidos = () => {
+  const formatador = Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' })
+
+  const [pedidos, setPedidos] = useState<IPedido[]>([])
+
+  useEffect(() => {
+    const token = useObterToken()
+    axios
+      .get<IPedido[]>('http://localhost:8000/pedidos', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(resposta => setPedidos(resposta.data))
+      .catch(erro => console.log(erro))
+  }, [])
+
+  const excluir = (pedido: IPedido) => {
+    alert(`Você excluiu o pedido ${pedido.id}`)
+    const token = useObterToken()
+    axios
+      .delete(`http://localhost:8000/pedidos/${pedido.id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(() => {
+        setPedidos(pedidos.filter(p => p.id !== pedido.id))
+      })
+      .catch(erro => console.log(erro))
+  }
+
   return (
     <section className='pedidos'>
       <h1>Meus Pedidos</h1>
-      <div>
-        <ul>
-          <li>
-            Pedido: <strong>1234</strong>
-          </li>
-          <li>
-            Data do pedido: <strong>22/02/2023</strong>
-          </li>
-          <li>
-            Valor total: <strong>R$ 42.00</strong>{' '}
-          </li>
-          <li>
-            Entrega realizada em: <strong>23/02/2023</strong>
-          </li>
-        </ul>
-        <AbBotao texto='Detalhes' />
-      </div>
+      {pedidos.map(pedido => (
+        <div className='pedido' key={pedido.id}>
+          <ul>
+            <li>
+              Pedido: <strong>{pedido.id}</strong>
+            </li>
+            <li>
+              Data do pedido:
+              <strong> {new Date(pedido.data).toLocaleDateString()}</strong>
+            </li>
+            <li>
+              Valor total: <strong>{formatador.format(pedido.total)}</strong>
+            </li>
+            <li>
+              Entrega realizada em:
+              <strong> {new Date(pedido.entrega).toLocaleDateString()}</strong>
+            </li>
+          </ul>
+          <AbBotao texto='Excluir' tipo='secundario' onClick={() => excluir(pedido)} />
+          <AbBotao texto='Detalhes' />
+        </div>
+      ))}
     </section>
   )
 }
