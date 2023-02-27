e12f5ca0e9d432adb7077dc81ae26f22fb3bfbe8
ilonavarro <ilo.navarro+github@gmail.com>
2023-02-27 15:41:43 +0000

    ra react queries: loader ok

--- curso-react-alurabooks-main/src/paginas/Categoria/index.tsx
+++ curso-react-alurabooks-main/src/paginas/Categoria/index.tsx
@@ -1,23 +1,37 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import Loader from '../../componentes/Loader'
 import TituloPrincipal from '../../componentes/TituloPrincipal'
 import http from '../../http'
 import { ICategoria } from '../../interfaces/ICategoria'
 
 const Categoria = () => {
   const [categoria, setCategoria] = useState<ICategoria>()
+  const [estaCarregando, setEstaCarregando] = useState(true)
 
   const params = useParams()
 
   useEffect(() => {
+    setEstaCarregando(true)
     http
       .get<ICategoria[]>('categorias', {
         params: {
           slug: params.slug
         }
       })
-      .then(resposta => setCategoria(resposta.data[0]))
+      .then(resposta => {
+        setCategoria(resposta.data[0])
+        setEstaCarregando(false)
+      })
+      .catch(erro => {
+        alert(`Ocorreu um erro: ${erro}`)
+        setEstaCarregando(false)
+      })
   }, [params.slug])
+
+  if (estaCarregando) {
+    return <Loader />
+  }
 
   return (
     <section>
